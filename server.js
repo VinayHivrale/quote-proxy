a232d4981597bc19ac67c2290a18891114e44c5a
VinayHivrale <102036688+VinayHivrale@users.noreply.github.com>
2024-08-15 19:53:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const port = 3000;
 
 app.use(cors());
 
+
+
+app.get('/', async (req, res) => {
+  try {
+    // Your asynchronous operations (if any) go here
+
+    res.json({ msg: "hello" });
+  } catch (error) {
+    // Handle any errors that occurred during the async operations
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 app.get('/api/quote', async (req, res) => {
   try {
     const response = await axios.get('https://zenquotes.io/api/today');
@@ -15,6 +30,7 @@ app.get('/api/quote', async (req, res) => {
     res.status(500).send('Failed to fetch quote.');
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Proxy server running at http://localhost:${port}`);
